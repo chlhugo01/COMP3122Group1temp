0c3a419d1095a658b19416999903a8de91fd3238
him <18062182A@student.hkcc-polyu.edu.hk>
2021-11-14 17:48:36 +0800

    update customer and restaurant database

--- restaurant_order/mongo-init.js
+++ restaurant_order/mongo-init.js
@@ -1,31 +1,31 @@
 db.auth('comp3122', '23456')
-db = db.getSiblingDB('customer_orders')
+db = db.getSiblingDB('restaurant_orders')
 
 db.createCollection('Order')
 db.Order.insertOne({
-    'customer_id':1,
-    'name': 'charlie',
+    'restaurant_id':1,
+    'name': 'Hong Kong Happy Dim Sum',
     'order':[
-        {'order_id':'r1o1', 'restaurant_id':1, 'food_id':1, 'deliver':1,'taken':true},
-        {'order_id':'r2o1', 'restaurant_id':2, 'food_id':3, 'deliver':3,'taken':true},
-        {'order_id':'r2o3', 'restaurant_id':2, 'food_id':3, 'deliver':0,'taken':false},
+        {'order_id':'r1o1', 'customer_id':1, 'food_id':1, 'perpare':true,'deliver':1},
+        {'order_id':'r1o2', 'customer_id':3, 'food_id':1, 'perpare':true,'deliver':3},
+        {'order_id':'r1o3', 'customer_id':2, 'food_id':2, 'perpare':false,'deliver':0},
     ]
 })
 db.Order.insertOne({
-    'customer_id':2,
-    'name': 'chloe',
+    'restaurant_id':2,
+    'name': 'Hong Kong Happy Meal',
     'order':[
-        {'order_id':'r3o1', 'restaurant_id':3, 'food_id':3, 'deliver':2,'taken':true},
-        {'order_id':'r2o2', 'restaurant_id':2, 'food_id':2, 'deliver':1,'taken':true},
-        {'order_id':'r1o3', 'restaurant_id':1, 'food_id':2, 'deliver':0,'taken':false},
+        {'order_id':'r2o1', 'customer_id':1, 'food_id':3, 'perpare':true, 'deliver':3},
+        {'order_id':'r2o2', 'customer_id':2, 'food_id':2, 'perpare':true, 'deliver':1},
+        {'order_id':'r2o3', 'customer_id':1, 'food_id':3, 'perpare':false, 'deliver':0},
     ]
 })
 db.Order.insertOne({
-    'customer_id':3,
-    'name': 'charlotte',
+    'restaurant_id':3,
+    'name': 'Hong Kong Happy Restaurant',
     'order':[
-        {'order_id':'r3o2', 'restaurant_id':3, 'food_id':2, 'deliver':2,'taken':true},
-        {'order_id':'r1o2', 'restaurant_id':1, 'food_id':1, 'deliver':0,'taken':false},
-        {'order_id':'r3o3', 'restaurant_id':3, 'food_id':3, 'deliver':0,'taken':false},
+        {'order_id':'r3o1', 'customer_id':2, 'food_id':3, 'perpare':true,'deliver':2},
+        {'order_id':'r3o2', 'customer_id':3, 'food_id':2, 'perpare':true,'deliver':2},
+        {'order_id':'r3o3', 'customer_id':3, 'food_id':3, 'perpare':false,'deliver':0},
     ]
 })
